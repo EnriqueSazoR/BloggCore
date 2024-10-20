e8025f03d59858c6b71486d6650974074c9a0a0e
EnriqueSazoR <luisrosales9598@gmail.com>
2024-10-20 17:40:54 -0600

    Se agregó unidad de trabajo y se creo la vista de crear categorias

---
+++ BlogCore.AccesoDatos/Data/Repository/IRepository/IContenedorTrabajo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository.IRepository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        // Aquí se deben agregar los repositorios
+        ICategoriaRepository Categoria { get; }
+
+        void Save();
+    }
+}
--- BlogCore.AccesoDatos/obj/Debug/net8.0/BlogCore.AccesoDatos.AssemblyInfo.cs
+++ BlogCore.AccesoDatos/obj/Debug/net8.0/BlogCore.AccesoDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogCore.AccesoDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07958e41156f7f0f8ef5e8efdd2a7a7e6e19836d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogCore.AccesoDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogCore.AccesoDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
